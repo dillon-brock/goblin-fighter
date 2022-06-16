7b31295a586c7ce349cc55899e415d7e1d948ee6
Dillon Brock <mdillon1297@gmail.com>
2022-06-16 08:56:50 -0700

    handleAddGoblin form event listener set up

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 // import services and utilities
-import state from './state.js';
+import { getRandomItem } from './utils.js';
+import state, { addGoblin } from './state.js';
 // import component creators
 import createCharacter from './components/Character.js';
+import createAddGoblin from './components/AddGoblin.js';
 // import state and dispatch functions
 
 // Create each component: 
 // - pass in the root element via querySelector
 // - pass any needed handler functions as properties of an actions object 
 const CreateCharacter = createCharacter(document.getElementById('character-display'));
+const CreateAddGoblin = createAddGoblin(document.querySelector('form'), {
+    handleAddGoblin: (name) => {
+        const goblin = {
+            name,
+            points: getRandomItem(state.goblinPoints),
+            defeated: false
+        };
+        addGoblin(goblin);
+        display();
+    }
+});
 
 // Roll-up display function that renders (calls with state) each component
 function display() {
     // Call each component passing in props that are the pieces of state this component needs
     CreateCharacter({ healthPoints: state.healthPoints });
+    CreateAddGoblin();
+
 }
 
 // Call display on page load
---
+++ components/AddGoblin.js
@@ -0,0 +1,12 @@
+export default function createAddGoblin(form, { handleAddGoblin }) {
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault;
+        const formData = new FormData(form);
+        handleAddGoblin(formData.get('name'));
+        form.reset();
+    });
+    return () => {
+    
+    };
+}
